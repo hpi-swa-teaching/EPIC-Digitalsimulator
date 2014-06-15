a6637373aeb46a43ca0f22530439d7d244a2b25e
adius <mail@adriansieber.com>
2014-06-15 13:55:57 +0200

    Added build files, fixed installation script

--- install.sh
+++ install.sh
@@ -1,42 +1,51 @@
 #! /bin/bash
 
+set -e
+
 export ST="Squeak-4.5"
 export PROJECT_HOME="$(pwd)"
 
-mkdir -p Digitalsimulator
-cd Digitalsimulator
+APP_NAME="Digitalsimulator"
 
-mkdir 'images'
-cd 'images'
+mkdir -p $APP_NAME
+cd $APP_NAME
 
-mkdir -p Squeak4.5
-cd Squeak4.5
+# mkdir 'images'
+# cd 'images'
+# 
+# mkdir -p Squeak4.5
+# cd Squeak4.5
+# 
+# wget http://ftp.squeak.org/4.5/Squeak4.5-13680.zip
+# unzip Squeak4.5-13680.zip
+# rm Squeak4.5-13680.zip
+# 
+# wget http://ftp.squeak.org/4.5/SqueakV41.sources.gz
+# gunzip SqueakV41.sources.gz
+# 
+# IMAGE_BASE_NAME=Squeak4.5-13680
 
-wget http://ftp.squeak.org/4.5/Squeak4.5-13680.zip
-unzip Squeak4.5-13680.zip
-rm Squeak4.5-13680.zip
 
-wget http://ftp.squeak.org/4.5/SqueakV41.sources.gz
-gunzip SqueakV41.sources.gz
-rm SqueakV41.sources.gz
+# TODO: Create .image, .changes and .sources from source
 
-IMAGE_BASE_NAME=Squeak4.5-13680
+cd $PROJECT_HOME
 
-# TODO: Create .image, .changes and .sources
-
-mv *.image Squeak-All-In-One
-mv *.changes Squeak-All-In-One
-mv *.sources Squeak-All-In-One
+cp build/*.image Squeak-All-In-One
+cp build/*.changes Squeak-All-In-One
+cp build/*.sources Squeak-All-In-One
 
 cd Squeak-All-In-One
 
-./createNewAllInOne 4.5 3000 14.23
+./createNewAllInOne $APP_NAME 4.5 3000 14.23
 
-cd Digitalsimulator.app
+cd $APP_NAME.app
 
 if [[ $(uname) == 'Darwin' ]]
 then
-    ./Contents/MacOS/Resources/Squeak ./Contents/Resources/Squeak.image ../../applyLockDown.st
+    ./Contents/MacOS/Squeak ./Contents/Resources/Squeak.image ../../applyLockDown.st
 else
     ./Contents/Linux-i686/bin/squeak ./Contents/Resources/Squeak.image ../../applyLockDown.st
 fi
+
+cd ..
+mv $APP_NAME.app ../
